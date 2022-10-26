8823bbdf98e88252f4901c9bbbabb8d997008cb0
danyr <phoenixdhr@gmail.com>
2022-10-26 15:16:03 -0500

    se agrego .sol

--- Contratos/CreadorDeContratosSwapAutomactico.sol
+++ Contratos/CreadorDeContratosSwapAutomactico.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.7.0 <=0.9.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "./SwapInstantaneo";
+import "./SwapInstantaneo.sol";
 
 contract CreadorDeContratosSwapAutomactico {
 
